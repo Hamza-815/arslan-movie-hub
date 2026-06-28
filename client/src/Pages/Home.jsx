06a705f13897c22ea800f63c2aee8f4d94a85891
Hamza-815 <mhamzashoukat8181@gmail.com>
2026-06-28 23:31:28 +0500

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,1 +1,11 @@
 
+function Home() {
+  return (
+    <div style={{ padding: 20, color: "white" }}>
+      <h1>🎬 Arslan Movie Hub</h1>
+      <h2>Trending Movies</h2>
+    </div>
+  );
+}
+
+export default Home;
