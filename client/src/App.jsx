d0d54535fde9ff352aae4d8af3283f731d40a7f0
Hamza-815 <mhamzashoukat8181@gmail.com>
2026-06-28 22:52:57 +0500

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        minHeight: "100vh",
+        padding: "20px"
+      }}
+    >
+      <h1>🎬 Arslan Movie Hub</h1>
+      <p>Netflix Style Movie Streaming Platform</p>
+    </div>
+  );
+}
+
+export default App;
