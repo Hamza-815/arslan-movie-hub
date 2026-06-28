65815c2204c47d7448719259a01d234996af5aee
Hamza-815 <mhamzashoukat8181@gmail.com>
2026-06-28 23:32:57 +0500

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        background: "#111",
+        padding: "15px",
+        display: "flex",
+        gap: "20px"
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
