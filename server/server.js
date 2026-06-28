4ab871fc724e03c4cfd9a3522e7d5eb47610bc85
Hamza-815 <mhamzashoukat8181@gmail.com>
2026-06-29 01:36:47 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.get("/", (req, res) => {
     message: "Arslan Movie Hub API Running"
   });
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
